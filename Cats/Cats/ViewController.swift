5e8f9b56a51f11a7b500519083a4687aa4905e72
JoyceMatos <j.matosjoyce@gmail.com>
2016-10-05 19:53:08 -0400

    TableView Fun

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,7 +10,39 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    let catBreeds = [
+        "Abyssinian",
+        "American Bobtail",
+        "Balinese",
+        "Bengal Cats",
+        "Birman",
+        "Chartreux",
+        "Chinese Li Hua",
+        "Devon Rex",
+        "Havana Brown",
+        "Himalayan",
+        "Japanese Bobtail",
+        "Javanese",
+        "Korat",
+        "Maine CoonManx",
+        "Nebelung",
+        "Norwegian Forest"
+    ]
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return catBreeds.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "kittyCell", for: indexPath)
+        let cat = catBreeds[indexPath.row]
+        cell.textLabel?.text = cat
+        return cell
+    }
    
     
     
